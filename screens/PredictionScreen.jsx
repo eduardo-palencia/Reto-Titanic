335bf1955a241660c5d09b25f8a677017ebf7c3c
Carlos Olivas <carlosgolivas@gmail.com>
2022-09-14 13:35:58 -0500

    Prediction screen

--- screens/PredictionScreen.jsx
+++ screens/PredictionScreen.jsx
@@ -8,28 +8,35 @@ import axios from 'axios';
 
 export default function PredictionScreen() {
   const navigation = useNavigation();
-  const [prediction, setPrediction] = useState('');
-
-  const styles = StyleSheet.create({
-    input: {}
-  });
+  const [lg, setLg] = useState('');
+  const [rf, setRf] = useState('');
+  const [show, setShow] = useState(0);
 
   return (
     <SafeAreaView className='flex-1'>
       <Formik
-        initialValues={{ sepalLength: '', sepalWidth: '', petalLength: '', petalWidth: '' }}
+        initialValues={{ Sex: 0, Pclass: 0, SibSp: 0, Embarked: 0 }}
         //onSubmit = {value => console.log(values)}
-        onSubmit={(values) =>
+        onSubmit={(values) => {
+          values.Sex = parseInt(values.Sex);
+          values.Pclass = parseInt(values.Pclass);
+          values.SibSp = parseInt(values.SibSp);
+          values.Embarked = parseInt(values.Embarked);
+
           axios
-            .post('http://10.48.230.154:5000//predict', {
-              sepalLength: values.sepalLength,
-              sepalWidth: values.sepalWidth,
-              petalLength: values.petalLength,
-              petalWidth: values.petalWidth
+            .post('http://10.48.207.114:8080/predict', {
+              Sex: values.Sex,
+              Pclass: values.Pclass,
+              SibSp: values.SibSp,
+              Embarked: values.Embarked
             })
-            .then((response) => setPrediction(response.data))
-            .catch((error) => console.log(error))
-        }
+            .then((response) => {
+              setLg(response.data.lg);
+              setRf(response.data.rf);
+              setShow(1);
+            })
+            .catch((error) => console.log(error));
+        }}
       >
         {({ handleChange, handleBlur, handleSubmit, values }) => (
           <ScrollView>
@@ -37,39 +44,26 @@ export default function PredictionScreen() {
               <Text className='text-xl text-center font-bold py-4 text-white'>Predicción</Text>
             </View>
             <View className='px-4 pt-4'>
-              <Text className='py-2 font-bold'>Clase: </Text>
+              <Text className='py-4 font-bold'>Sexo: </Text>
+              <TextInput onChangeText={handleChange('Sex')} onBlur={handleBlur('Sex')} value={values.Sex} />
+              <Text className='py-4 font-bold'> Clase: </Text>
+              <TextInput onChangeText={handleChange('Pclass')} onBlur={handleBlur('Pclass')} value={values.Pclass} />
+              <Text className='py-4 font-bold'> Familiares: </Text>
+              <TextInput onChangeText={handleChange('SibSp')} onBlur={handleBlur('SibSp')} value={values.SibSp} />
+              <Text className='py-4 font-bold'> Embarco en: </Text>
               <TextInput
-                onChangeText={handleChange('sepalLength')}
-                onBlur={handleBlur('sepalLength')}
-                value={values.sepalLength}
-                style={styles.input}
-              />
-              <Text className='py-2 font-bold'>Sexo: </Text>
-              <TextInput
-                onChangeText={handleChange('sepalWidth')}
-                onBlur={handleBlur('sepalWidth')}
-                value={values.sepalWidth}
-              />
-              <Text className='py-2 font-bold'>Class: </Text>
-              <TextInput
-                onChangeText={handleChange('petalLength')}
-                onBlur={handleBlur('petalLength')}
-                value={values.petalLength}
-              />
-              <Text className='py-2 font-bold'>Class: </Text>
-              <TextInput
-                onChangeText={handleChange('petalWidth')}
-                onBlur={handleBlur('petalWidth')}
-                value={values.petalWidth}
+                onChangeText={handleChange('Embarked')}
+                onBlur={handleBlur('Embarked')}
+                value={values.Embarked}
               />
             </View>
 
             <View className='flex-row justify-center py-4'>
               <Button onPress={handleSubmit} title='Submit' color='darkblue' />
             </View>
-
-            <View className='flex-row justify-center py-4 bg-gray-200 rounded-md'>
-              <Text style={{ fontSize: 20, color: 'darkblue' }}> La Predicción es: {prediction}</Text>
+            <View className={show ? 'py-4 bg-gray-200' : 'py-4 bg-gray-200 hidden'}>
+              <Text style={{ fontSize: 20, color: 'red' }}> Logistic Regression: {lg ? 'Vive :)' : 'Muere :('}</Text>
+              <Text style={{ fontSize: 20, color: 'red' }}> Random Forrest: {rf ? 'Vive :)' : 'Muere :('}</Text>
             </View>
           </ScrollView>
         )}
